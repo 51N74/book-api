03b66169d42f2e05090fc32c4a0a6e76c2717eb3
51N72 <j.pattanasintara@gmail.com>
2025-05-06 16:20:32 +0700

    add_admin_id_on_books

---
+++ src/infrastructure/postgres/migrations/2025-05-06-091523_add_admin_id_on_books/down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS books_categories_junction CASCADE;
+DROP TABLE IF EXISTS admin CASCADE;
+Drop TABLE IF EXISTS book_user_junction CASCADE;
